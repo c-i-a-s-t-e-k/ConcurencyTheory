258c582004a5da0676bbef3623e61f1883063ca0
Dominik <jezowdominik@duck.com>
2023-10-20 09:33:52 +0200

    lab1

---
+++ lab1/src/MyRun.java
@@ -0,0 +1,17 @@
+class MyRun implements Runnable {
+    private final int id;
+
+    public MyRun(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Watek " + id);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ lab1/src/MyThread.java
@@ -0,0 +1,17 @@
+class MyThread extends Thread {
+    private int id;
+
+    public MyThread(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+            System.out.println("Watek " + id);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+    }
+}
