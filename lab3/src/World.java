912271652598bc46686edaae91c8084d770c095b
Dominik <jezowdominik@duck.com>
2023-11-17 09:37:24 +0100

    2 cond prod consumers

--- lab3/src/World.java
+++ lab3/src/World.java
@@ -19,21 +19,26 @@ public class World {
         try {
             lock.lock();
             System.out.println(person.introduce() + " want to produce " + toProduce + " Value = " + this.value);
-            while (!isFirstProducerEmpty) {
-                System.out.println(person.introduce() + " is waiting on Producer\n");
-                producerCond.await();
-            }
+//            while (!isFirstProducerEmpty) {
+//                System.out.println(person.introduce() + " is waiting on Producer\n");
+//                producerCond.await();
+//            }
+//            while (!canProduce(toProduce)) {
+//                System.out.println(person.introduce() + " is waiting on first Producers\n");
+//                isFirstProducerEmpty = false;
+//                firstProducerCond.await();
+//            }
+
             while (!canProduce(toProduce)) {
-                System.out.println(person.introduce() + " is waiting on first Producers\n");
-                isFirstProducerEmpty = false;
-                firstProducerCond.await();
+                System.out.println(person.introduce() + " is waiting on producers\n");
+                producerCond.await();
             }
             this.value += toProduce;
             System.out.println(person.introduce() + " Produced " + toProduce + "\n");
 
-            isFirstConsumerEmpty = true;
+//            isFirstConsumerEmpty = true;
             consumerCond.signal();
-            firstConsumerCond.signal();
+//            firstConsumerCond.signal();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
@@ -45,21 +50,26 @@ public class World {
         try {
             lock.lock();
             System.out.println(person.introduce() + " want to consume " + toConsume + " Value = " + this.value);
-            while (!isFirstConsumerEmpty) {
-                System.out.println(person.introduce() + " is waiting on Consumer\n");
-                consumerCond.await();
-            }
+//            while (!isFirstConsumerEmpty) {
+//                System.out.println(person.introduce() + " is waiting on Consumer\n");
+//                consumerCond.await();
+//            }
+//            while (!canConsume(toConsume)) {
+//                System.out.println(person.introduce() + " is waiting on first Consumers\n");
+//                isFirstConsumerEmpty = false;
+//                firstConsumerCond.await();
+//            }
             while (!canConsume(toConsume)) {
-                System.out.println(person.introduce() + " is waiting on first Consumers\n");
-                isFirstConsumerEmpty = false;
-                firstConsumerCond.await();
+                System.out.println(person.introduce() + " is waiting on Consumers\n");
+//                isFirstConsumerEmpty = false;
+                consumerCond.await();
             }
             this.value -= toConsume;
             System.out.println(person.introduce() + " Consumed " + toConsume + "\n");
 
-            isFirstProducerEmpty = true;
+//            isFirstProducerEmpty = true;
             producerCond.signal();
-            firstProducerCond.signal();
+//            firstProducerCond.signal();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
